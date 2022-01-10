c017700ae1041703bd8f9505667c5841d935be70
ShunichirouKamino <kamino.shunichirou@isid.co.jp>
2022-01-10 18:32:25 +0900

    日付クレートを追加

--- todo_app/src/cli.rs
+++ todo_app/src/cli.rs
@@ -4,16 +4,22 @@ use structopt::StructOpt;
 /// # Actionの直和型
 ///
 /// コマンドライン引数を受け取り、それにともなう操作を直和型で定義します。
+/// ドキュメンテーションコメントにより、help表示するコメントを記載することができます。
+///
 #[derive(Debug, StructOpt)]
 pub enum Action {
+    /// Write tasks to the journal file.
     Add {
+        /// The task description text.
         #[structopt()]
         text: String,
     },
+    /// Remove an entry from the journal file by position.
     Done {
         #[structopt()]
         position: usize,
     },
+    /// List all tasks in the journal file.
     List,
 }
 
@@ -26,6 +32,7 @@ pub struct CommandLineArgs {
     #[structopt(subcommand)]
     pub action: Action,
 
+    /// Use a different journal file.
     #[structopt(parse(from_os_str), short, long)]
     pub journal_file: Option<PathBuf>,
 }
