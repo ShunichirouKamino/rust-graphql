40f5fca739f2e428a4b11481ee4fd9f8dead7410
ShunichirouKamino <kamino.shunichirou@isid.co.jp>
2022-01-10 22:14:18 +0900

    不足しているuseを追加

--- todo_app/src/tasks.rs
+++ todo_app/src/tasks.rs
@@ -1,4 +1,6 @@
-use chrono::{DateTime, Utc};
+use chrono::{serde::ts_seconds, DateTime, Local, Utc};
+use serde::Deserialize;
+use serde::Serialize;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
