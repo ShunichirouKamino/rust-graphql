80510ac56d531e9c6269e7b2c9c83191566db318
ShunichirouKamino <kamino.shunichirou@isid.co.jp>
2022-01-12 18:10:39 +0900

    mainにてコマンドライン引数を取得するように修正し、発行されるjsonをignore

--- todo_app/src/main.rs
+++ todo_app/src/main.rs
@@ -5,9 +5,24 @@ mod cli;
 mod tasks;
 use structopt::StructOpt;
 
+use cli::{Action::*, CommandLineArgs};
+use tasks::Task;
+
 /// # 関数に付与するコメントです。
 ///
 /// メインエントリです。
 fn main() {
-    println!("{:#?}", cli::CommandLineArgs::from_args());
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    let journal_file = journal_file.expect("Failed to find journal file");
+
+    match action {
+        Add { text } => tasks::add_task(journal_file, Task::new(text)),
+        List => tasks::list_tasks(journal_file),
+        Done { position } => tasks::complete_task(journal_file, position),
+    }
+    .expect("Faild to perform action")
 }
