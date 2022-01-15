549da510352063da2f36708e03d63e2a3ce01193
ShunichirouKamino <kamino.shunichirou@isid.co.jp>
2022-01-15 14:05:51 +0000

    anyhowの追加によるエラー文修正

--- todo_app/src/main.rs
+++ todo_app/src/main.rs
@@ -3,16 +3,17 @@
 
 mod cli;
 mod tasks;
-use structopt::StructOpt;
 
+use anyhow::anyhow;
 use cli::{Action::*, CommandLineArgs};
 use std::path::PathBuf;
+use structopt::StructOpt;
 use tasks::Task;
 
 /// # 関数に付与するコメントです。
 ///
 /// メインエントリです。
-fn main() {
+fn main() -> anyhow::Result<()> {
     let CommandLineArgs {
         action,
         journal_file,
@@ -20,14 +21,14 @@ fn main() {
 
     let journal_file = journal_file
         .or_else(find_default_journal_file)
-        .expect("Failed to find journal file");
+        .ok_or(anyhow!("Failed to find journal file."))?;
 
     match action {
         Add { text } => tasks::add_task(journal_file, Task::new(text)),
         List => tasks::list_tasks(journal_file),
         Done { position } => tasks::complete_task(journal_file, position),
-    }
-    .expect("Faild to perform action")
+    }?;
+    Ok(())
 }
 
 /// # journalfile検索
