dd20cd6cd9570a2a7f18c546144c98097113e63e
ShunichirouKamino <kamino.shunichirou@isid.co.jp>
2022-01-16 09:25:53 +0000

    割り勘アプリのガワ作成

---
+++ dutch_treat/src/cli.rs
@@ -0,0 +1,41 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+/// # Actionの直和型
+///
+/// コマンドライン引数を受け取り、それにともなう操作を直和型で定義します。
+/// ドキュメンテーションコメントにより、help表示するコメントを記載することができます。
+///
+#[derive(Debug, StructOpt)]
+pub enum Action {
+    /// Write tasks to the journal file.
+    Add {
+        /// The task description text.
+        #[structopt()]
+        name: String,
+
+        #[structopt()]
+        years: usize,
+    },
+    /// Remove an entry from the journal file by position.
+    Calc {
+        #[structopt()]
+        position: usize,
+    },
+    /// List all tasks in the journal file.
+    List,
+}
+
+/// # コマンドライン引数を読み取る構造体
+///
+/// Actionのラッパーとして起動し、subcommandで入力された型でActionをインスタンス化します。
+#[derive(Debug, StructOpt)]
+#[structopt(name = "to-do app", about = "A command line to-do app written in Rust")]
+pub struct CommandLineArgs {
+    #[structopt(subcommand)]
+    pub action: Action,
+
+    /// Use a different journal file.
+    #[structopt(parse(from_os_str), short, long)]
+    pub journal_file: Option<PathBuf>,
+}
--- dutch_treat/src/main.rs
+++ dutch_treat/src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+//! # todoアプリです。
+//!
+
+mod cli;
+mod tasks;
+
+use anyhow::anyhow;
+use cli::{Action::*, CommandLineArgs};
+use std::path::PathBuf;
+use structopt::StructOpt;
+use tasks::Task;
+
+/// # 関数に付与するコメントです。
+///
+/// メインエントリです。
+fn main() -> anyhow::Result<()> {
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    let journal_file = journal_file
+        .or_else(find_default_journal_file)
+        .ok_or(anyhow!("Failed to find journal file."))?;
+
+    match action {
+        Add { name, years } => println!("{}, {}", name, years),
+        // Add { text } => tasks::add_task(journal_file, Task::new(text)),
+        // List => tasks::list_tasks(journal_file),
+        // Done { position } => tasks::complete_task(journal_file, position),
+    }?;
+    Ok(())
+}
+
+/// # journalfile検索
+///
+/// - homeディレクトリを検索します
+/// - homeディレクトリに、journalファイルの名称を付与して返却します
+fn find_default_journal_file() -> Option<PathBuf> {
+    let journal_file = ".rusty-journal.json";
+    let pusher = |mut path: PathBuf| {
+        path.push(journal_file);
+        println!("{:?}", path);
+        path
+    };
+    home::home_dir().map(pusher)
 }
