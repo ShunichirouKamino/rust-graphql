7ec37103db8d574a7b0cdebfe97a6720fe584ed7
ShunichirouKamino <kamino.shunichirou@isid.co.jp>
2022-04-21 16:02:07 +0000

    reqestのカスタムエラー追加

--- idp/src/main.rs
+++ idp/src/main.rs
@@ -3,7 +3,11 @@
 mod hello_html;
 mod hello_rest;
 
-use actix_web::{middleware, web, App, HttpServer};
+use actix_web::{
+    error, middleware,
+    web::{self, JsonConfig},
+    App, HttpResponse, HttpServer,
+};
 use hello_html::hello_html_handler;
 use hello_rest::hello_rest_handler;
 
@@ -16,6 +20,15 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Logger::default())
+            .app_data(
+                web::JsonConfig::default()
+                    .limit(4096) // limit request payload size
+                    .content_type(|mime| mime == mime::TEXT_PLAIN) // only accept text/plain content type
+                    .error_handler(|err, req| {
+                        error::InternalError::from_response(err, HttpResponse::Conflict().into())
+                            .into()
+                    }), // use custom error handler
+            )
             .service(hello_html_handler)
             .service(web::resource("/rest").route(web::post().to(hello_rest_handler)))
     })
