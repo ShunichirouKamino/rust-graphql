2e4d474da0594a9ec70f257ca73dd0b8beecfe07
ShunichirouKamino <kamino.shunichirou@isid.co.jp>
2022-04-21 15:06:07 +0000

    loggerの追加

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    let list: Vec<&str> = "hello,world,goodbye,world".split(',').collect();
+    let elem = list.get(0).unwrap();
+
+    println!("{}", elem);
 }
--- idp/src/main.rs
+++ idp/src/main.rs
@@ -3,14 +3,19 @@
 mod hello_html;
 mod hello_rest;
 
-use actix_web::{App, HttpServer};
+use actix_web::{middleware, App, HttpServer};
 use hello_html::hello_html_handler;
 use hello_rest::hello_rest_handler;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    log::info!("starting HTTP server at http://localhost:8080");
+
     HttpServer::new(|| {
         App::new()
+            .wrap(middleware::Logger::default())
             .service(hello_html_handler)
             .service(hello_rest_handler)
     })
